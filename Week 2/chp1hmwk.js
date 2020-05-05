66d335f404e331bceffcfa459ac144d001592123
Erica Hill <acireh92@gmail.com>
2020-05-05 09:00:00 -0500

    variables

---
+++ Week 2/chp1hmwk.js
@@ -0,0 +1,15 @@
+//create 10 variables. All have to have different values.
+// Once you have created the variables, create one last
+//variable with all the varables added together.
+// HINT: use the + sign to add the varables.
+
+var name = Erica
+var birthday = March 17
+var age = 28
+var carName = Volkwagon
+var state = Missouri
+var color = Pink
+var food = Pizza
+var season = Autumn
+var year = 2020
+var store = Target
