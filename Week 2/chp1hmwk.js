067623d0d62699009e6ca79b7ba2892d5fa1d303
Erica Hill <acireh92@gmail.com>
2020-05-11 10:50:54 -0500

    new week new code

--- Week 2/chp1hmwk.js
+++ Week 2/chp1hmwk.js
@@ -3,13 +3,32 @@
 //variable with all the varables added together.
 // HINT: use the + sign to add the varables.
 
-var name = Erica
-var birthday = March 17
-var age = 28
-var carName = Volkwagon
-var state = Missouri
-var color = Pink
-var food = Pizza
-var season = Autumn
-var year = 2020
-var store = Target
+var name = "Erica"
+var birthday = "March 17"
+var age = "28"
+var carName = "Volkwagon"
+var state = "Missouri"
+var color = "Pink"
+var food = "Pizza"
+var season = "Autumn"
+var year = "2020"
+var store = "Target"
+
+
+
+//Global Variable - variables all by themselves
+// Not insdie a scope or block : { <--scope/block --> }
+var someGuy = "John";
+
+var dayofParty = 23;
+
+var lastGlobal = "Just Relax";
+
+// Local Variables-variables belong to something
+// inside a scope or block : { var a = 1; }
+function simple (){
+    var a = 1; // Local varable
+}
+for(var i = 0;i < 10; i++){
+    console.log(i)
+}
---
+++ Week 3/variables.js
@@ -0,0 +1,41 @@
+// First we need the Var keyword
+var thisVariable;
+document.getElementById("demo").innerHTML=thisVariable;
+
+var anotherOne;
+
+var thelLastTrain;
+
+// Next we need a name
+var myVariable ; // Declare a Variable
+
+// Next we need to add a Data Type
+myVariable = 100 ; // Instantiate value , set a value to myVariable
+
+// Change the value of myVariable .
+myVariable ="One Hundred";
+
+thisVariable = "money";
+document.getElementById("demo2").innerHTML=thisVariable;
+
+thisVariable = 3.33 ;
+document.getElementById("demo3").innerHTML=thisVariable;
+
+thisVariable = 45.64;
+document.getElementById("demo4").innerHTML=thisVariable;
+myVariable = 11e3;
+
+anotherOne = true
+
+thelLastTrain = [1,2,3,4,5,6];
+
+thelLastTrain = [ "one", "two", "three"];
+
+anotherOne = {
+    name: "erica",
+    FavColor: "pink"
+}
+
+thisVariable = undefined;
+document.getElementById("demo5").innerHTML=thisVariable;
+
