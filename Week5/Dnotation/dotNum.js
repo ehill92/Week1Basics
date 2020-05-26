01bff56f395f5cd75a6be9858e6640b88993a951
Erica Hill <acireh92@gmail.com>
2020-05-26 12:01:49 -0500

    Dot Notation

---
+++ Week5/Dnotation/dotNum.js
@@ -0,0 +1,37 @@
+var myNumber = 456;
+
+var result;
+
+result =myNumber.toString();
+
+console.log(result);
+
+result =myNumber.toPrecision();
+
+console.log(result);
+
+result =myNumber.toFixed();
+
+console.log(result);
+
+result =myNumber.toLocaleString();
+
+console.log(result);
+
+result =myNumber.toExponential();
+
+console.log(result);
+
+result =myNumber.valueOf();
+
+console.log(result);
+
+var mySecNum = '5e9';
+
+result = parseInt('Infinity');
+
+console.log(result)
+
+result = filterInt('Infinity');
+
+console.log(result)
---
+++ Week5/dotOperator.js
@@ -0,0 +1,28 @@
+//Create Object
+var person1 = {
+    name:"Erica",
+    age:28,
+    weight:128,
+    dob:"03/17/1992"
+    
+};
+var person2 = {};
+
+//Added Properties
+person1.name = "Erica";
+person2['name'] = "Kylen"
+
+//Print Object
+console.log(person1);
+console.log(person2);
+
+//Dot Notation
+// .
+
+//Bracket Notation
+// []
+
+console.log(person1['age']);
+console.log(person1.age);
+var date = person1.dob;
+
