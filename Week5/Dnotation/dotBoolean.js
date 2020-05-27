62a97a1d4b488fb0e636b5c35019eaa7cf9c04e2
Erica Hill <acireh92@gmail.com>
2020-05-27 11:21:31 -0500

    dates & bools

---
+++ Week5/Dnotation/dotBoolean.js
@@ -0,0 +1,12 @@
+var mybool = true;
+var result;
+
+result = mybool.toString();
+console.log(mybool);
+console.log(result);
+
+result = mybool.valueOf();
+console.log(result);
+
+result = mybool.toString().toUpperCase().repeat(15);
+console.log(result);
